40d26de990315e8c3922efdc509bc6c61f76aa2c
prj7465 <98361105+prj7465@users.noreply.github.com>
2022-10-18 16:36:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,32 @@
 def main():
   conn = get_conn()  # TODO
   
-  while True:
+  logged_in = False
+  
+  while not logged_in:
     print('Welcome to MvieLvrs tools application')
     inp = input('Enter l to login or c to create an account: ').lower()
     if inp == 'l':
-      print('Logging in')
+      print('Log in')
       username = input('Username: ')
       password = input('Password: ')
       if login_user(conn, username, password):
         print('Login successful')
+        logged_in = True
         break
       else:
         print('Incorrect login')
     elif inp == 'c':
       print('Creating user')
-      create_user(conn, username, password, first_name, last_name, email)
+      if create_user(conn, username, password, first_name, last_name, email):
+        print('Created successfully')
+        logged_in = True
+        break
+      else:
+        print('Error on creation')
     else:
       print('Unrecognized input')
+  
+  print('You are now logged in')
+  inp = input('Enter a command (h for help)').lower()
+  
