0457210f6140c23f4412c7b100727453f5e13f0f
JoseEstevez <jae9307@g.rit.edu>
2022-10-18 18:45:30 -0400

    created initial manage request function

--- borrow.py
+++ borrow.py
@@ -10,3 +10,16 @@ def borrow(conn, barcode, username, date, duration):
     if shareable:
         query = "insert into tools_req values(" + username + ", " + barcode + ", " + date + ", " + duration + ", Pending, " + datetime.now() + ", " + (date+duration)
         sql.execute_query(conn, query)
+
+
+def manage(conn, username, flag):
+    if flag == "g":
+        query = "select barcode, date_required from tools_req where username = " + username
+        requests = list(sql.read_query(conn, query))
+        for request in requests:
+            print(request)
+    else:
+        query = "select username, barcode, date_required from tools_req where barcode = select barcode from tools where username = " + username
+        requests = list(sql.read_query(conn, query))
+        for request in requests:
+            print(request)
