84c2f93ee1fed0583ca3b572c5be77e76da22f1d
prj7465 <98361105+prj7465@users.noreply.github.com>
2022-10-18 20:39:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,146 @@
 # Patrick
 
+from sshtunnel import SSHTunnelForwarder
+
+from login import *
+
 COMMAND_FLAGS = {
-	'help': (),
-	'quit': (),
-	'tool': ('v', 'a', 'e', 'd'),
-	'categ': ('v', 'a', 'e', 'd'),
-	'reqs': ('g', 'r'),
-	'search': ()
+    'help': (),
+    'quit': (),
+    'tool': ('v', 'a', 'e', 'd'),
+    'categ': ('v', 'a', 'e', 'd'),
+    'reqs': ('g', 'r'),
+    'search': ()
 }
 
+
 def main():
-	conn = get_conn()  # TODO
+    username = 'prj7465'
+    password = 'lintThespian@1'
+    dbName = 'p32001_17'
 
-	logged_in = False
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('localhost', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port,
+        }
 
-	while not logged_in:
-		print('Welcome to MvieLvrs tools application')
-		inp = input('Enter "login" to login or "new" to create an account: ').lower()
-		if inp == 'login':
-			print('Logging in')
-			username = input('Username: ')
-			password = input('Password: ')
-			if login_user(conn, username, password):
-				print('Login successful')
-				logged_in = True
-			else:
-				print('Incorrect login')
-		elif inp == 'new':
-			print('Creating user')
-			username = input('Username: ')
-			password = input('Password: ')
-			first_name = input('First Name: ')
-			last_name = input('Last Name: ')
-			email = input('Email: ')
-			
-			succ, e_type = create_user(conn, username, password, first_name, last_name, email):
-			if succ:
-				print('Created successfully')
-				logged_in = True
-			if e_type == 'Username':
-				print('Username already in use')
-			elif e_type == 'Email':
-				print('Email already in use')
-			else:
-				print('Error on creation')
-		else:
-			print('Unrecognized input')
-  
-	print('You are now logged in')
-	
-	while True:
-		print('Enter a command ("help" for help)')
-		inp = input('> ').lower()
-		command, *flags = inp.split()
-		if command not in COMMAND_FLAGS:
-			print('Unknown command - see "help"')
-		elif bool(flags) != bool(COMMAND_FLAGS[command]) or flags and flags[0] not in COMMAND_FLAGS[command]:
-			print('Invalid usage - see "help"')
-		elif command == 'help':
-			print('Commands:')
-			print('help             -  displays this menu')
-			print('quit             -  exits the program')
-			print('tool [v a e d]   -  manage your tools [view add edit delete]')
-			print('categ [v a e d]  -  manage your categories [view add edit delete]')
-			print('reqs [g r]       -  manage your borrow requests [given recieved]')
-			print('search           -  search for tool')
-		elif command == 'quit':
-			break
-		elif command == 'tool':
-			if flags[0] == 'v':
-				pass
-			elif flags[0] == 'a':
-				pass
-			elif flags[0] == 'e':
-				pass
-			elif flags[0] == 'd':
-				pass
-		elif command == 'categ':
-			if flags[0] == 'v':
-				pass
-			elif flags[0] == 'a':
-				pass
-			elif flags[0] == 'e':
-				pass
-			elif flags[0] == 'd':
-				pass
-		elif command == 'reqs':
-			if flags[0] == 'g':
-				pass
-			elif flags[0] == 'r':
-				pass
-		elif command == 'search':
-			pass
-  
-	print('Thanks for trusting Mvie Lovers!')
+        conn = psycopg2.connect(**params)
+        print("Database connection established")
+        logged_in = False
+
+        while not logged_in:
+            print('Welcome to MvieLvrs tools application')
+            inp = input(
+                'Enter "login" to login, "new" to create an account, or "quit" to quit: ').lower()
+            if inp == 'login':
+                print('Logging in')
+                username = input('Username: ')
+                password = input('Password: ')
+                if login_user(conn, username, password):
+                    print('Login successful')
+                    logged_in = True
+                else:
+                    print('Incorrect login')
+            elif inp == 'new':
+                print('Creating user')
+                username = input('Username: ')
+                if not username:
+                    print('Username cannot be empty')
+                    continue
+                password = input('Password: ')
+                if not password:
+                    print('Password cannot be empty')
+                    continue
+                first_name = input('First Name: ')
+                if not first_name:
+                    print('First Name cannot be empty')
+                    continue
+                last_name = input('Last Name: ')
+                if not last_name:
+                    print('Last Name cannot be empty')
+                    continue
+                email = input('Email: ')
+                if not email:
+                    print('Email cannot be empty')
+                    continue
+
+                succ, e_type = create_user(
+                    conn, username, password, first_name, last_name, email)
+                if succ:
+                    print('Created successfully')
+                    logged_in = True
+                elif e_type == 'Username':
+                    print('Username already in use')
+                elif e_type == 'Email':
+                    print('Email already in use')
+                else:
+                    print('Error on creation')
+            elif inp == 'quit':
+                break
+            else:
+                print('Unrecognized input')
+
+        if logged_in:
+            print('You are now logged in')
+
+        while logged_in:
+            print('Enter a command ("help" for help, "quit" to quit)')
+            inp = input('> ').lower()
+            command, *flags = inp.split()
+            if command not in COMMAND_FLAGS:
+                print('Unknown command - see "help"')
+            elif bool(flags) != bool(COMMAND_FLAGS[command]) or flags and flags[0] not in COMMAND_FLAGS[command]:
+                print('Invalid usage - see "help"')
+            elif command == 'help':
+                print('Commands:')
+                print('help             -  displays this menu')
+                print('quit             -  exits the program')
+                print(
+                    'tool [v a e d]   -  manage your tools [view add edit delete]')
+                print(
+                    'categ [v a e d]  -  manage your categories [view add edit delete]')
+                print(
+                    'reqs [g r]       -  manage your borrow requests [given recieved]')
+                print('search           -  search for tool')
+            elif command == 'quit':
+                break
+            elif command == 'tool':
+                if flags[0] == 'v':
+                    pass
+                elif flags[0] == 'a':
+                    pass
+                elif flags[0] == 'e':
+                    pass
+                elif flags[0] == 'd':
+                    pass
+            elif command == 'categ':
+                if flags[0] == 'v':
+                    pass
+                elif flags[0] == 'a':
+                    pass
+                elif flags[0] == 'e':
+                    pass
+                elif flags[0] == 'd':
+                    pass
+            elif command == 'reqs':
+                if flags[0] == 'g':
+                    pass
+                elif flags[0] == 'r':
+                    pass
+            elif command == 'search':
+                pass
+
+        print('Thanks for trusting Mvie Lovers!')
+
 
 if __name__ == '__main__':
-	main()
+    main()
