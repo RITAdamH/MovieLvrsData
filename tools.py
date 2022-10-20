8dd9c020b2d84b0656b97583f19eb0275e7a53c8
prj7465 <98361105+prj7465@users.noreply.github.com>
2022-10-19 21:13:56 -0400

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,15 @@
+from typing import Any
+
 from psycopg2.extensions import cursor
+
+
+def show_tool(tool: tuple[Any], categs: list[str] | None = None) -> None:
+    print(f'{tool[1]} [{tool[0]}]')
+    print(f'"{tool[2]}"')
+    print(f'Purchased on {tool[3]} (${tool[4]:.2f})')
+    print(f'{"Shareable" if tool[5] else "Not shareable"}')
+    print(f'Categories: {", ".join(categs) if categs is not None else ""}')
+    print()
 
 
 def show_tools(cur: cursor, username: str, by: str, ord: str) -> bool:
@@ -7,18 +18,20 @@ def show_tools(cur: cursor, username: str, by: str, ord: str) -> bool:
             cur.execute(
                 f"select * from tools where username = '{username}' order by name {'asc' if ord == 'a' else 'desc'}")
         else:
-            raise NotImplementedError  # TODO add sort by category
+            cur.execute(
+                f"select * from tools where username = '{username}' order by (select min(name) from categories where cid in (select cid from tool_categs where barcode = tools.barcode)) {'asc' if ord == 'a' else 'desc'} nulls last")
 
         tools = cur.fetchall()
 
         print(
             f'Your tools ({"category" if by == "c" else "name"} {"ascending" if ord == "a" else "descending"}):')
         for tool in tools:
-            print(f'{tool[1]} [{tool[0]}]')
-            print(f'"{tool[2]}"')
-            print(f'Purchased on {tool[3]} (${tool[4]:.2f})')
-            print(f'{"Shareable" if tool[5] else "Not shareable"}')
-            print()
+            cur.execute(
+                f"select name from categories where cid in (select cid from tool_categs where barcode = '{tool[0]}') order by name asc")
+
+            categs = cur.fetchall()
+
+            show_tool(tool, [categ[0] for categ in categs])
     except:
         return False
 
