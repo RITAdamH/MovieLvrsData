a3c91e30681970b690974a7d28ac1f9b0d2b3d4a
prj7465 <prj7465@rit.edu>
2022-11-08 14:46:03 -0500

    c

--- stats.py
+++ stats.py
@@ -31,25 +31,44 @@ def show_dashboard(cur: cursor, username: str) -> bool:
 
 
 def show_most_borrowed(cur: cursor, username: str) -> bool:
-    raise NotImplementedError
+    try:
+        cur.execute(
+            f"select tools.*, sum(coalesce(date_returned, current_date) - last_status_change + 1) as days_borrwed from tools, tool_reqs where tools.barcode = tool_reqs.barcode and tool_reqs.username = '{username}' and tool_reqs.status = 'Accepted' group by tools.barcode order by days_borrwed desc, name, barcode limit 10")
+
+        tools_borrowed_stats = cur.fetchall()
+
+        if not tools_borrowed_stats:
+            print('You have no borrow stats to show')
+        else:
+            print(
+                f'Your top ({len(tools_borrowed_stats)}) most borrowed tools [days borrowed descending]:')
+            for i, tool_borrowed_stats in enumerate(tools_borrowed_stats):
+                *tool, days_borrowed = tool_borrowed_stats
+                print(
+                    f'#{i + 1}. {days_borrowed} day(s)')
+                show_tool(cur, username, tool)
+    except:
+        return False
+
+    return True
 
 
 def show_most_lent(cur: cursor, username: str) -> bool:
     try:
         cur.execute(
-            f"with tools_lent_stats as (select tools.*, sum(coalesce(date_returned, current_date) - last_status_change + 1) as days_lent, current_date - tools.purchase_date as days_owned, avg(coalesce(date_returned, current_date) - last_status_change + 1)::numeric(8, 2) as avg_days_lent from tools, tool_reqs where tools.barcode = tool_reqs.barcode and tools.username = '{username}' and tool_reqs.status = 'Accepted' group by tools.barcode) select *, (days_lent::numeric / days_owned)::numeric(8, 4) as lent_pct from tools_lent_stats where days_lent is not null order by lent_pct desc, name, barcode limit 10")
+            f"with tools_lent_stats as (select tools.*, sum(coalesce(date_returned, current_date) - last_status_change + 1) as days_lent, current_date - tools.purchase_date as days_owned, avg(coalesce(date_returned, current_date) - last_status_change + 1)::numeric(8, 2) as avg_days_lent from tools, tool_reqs where tools.barcode = tool_reqs.barcode and tools.username = '{username}' and tool_reqs.status = 'Accepted' group by tools.barcode) select *, (days_lent::numeric / days_owned)::numeric(8, 4) as lent_pct from tools_lent_stats order by lent_pct desc, name, barcode limit 10")
 
         tools_lent_stats = cur.fetchall()
 
         if not tools_lent_stats:
-            print('You have not lent any tools')
+            print('You have no lend stats to show')
         else:
             print(
-                f'Your top ({len(tools_lent_stats)}) most lent tools [lent percentage descending]:')
+                f'Your top ({len(tools_lent_stats)}) most lent tools [percentage lent descending]:')
             for i, tool_lent_stats in enumerate(tools_lent_stats):
                 *tool, days_lent, days_owned, avg_days_lent, lent_pct = tool_lent_stats
                 print(
-                    f'#{i + 1}. {lent_pct:.2%} - lent for {days_lent}/{days_owned} day(s) owned ({avg_days_lent} day(s) on avg)')
+                    f'#{i + 1}. {lent_pct:.2%} - lent for {days_lent}/{days_owned} day(s) owned ({avg_days_lent:.2f} day(s) on avg)')
                 show_tool(cur, username, tool)
     except:
         return False
